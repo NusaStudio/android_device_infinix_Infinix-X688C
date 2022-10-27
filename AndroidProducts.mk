3e36ea1d15b4407f0c344a631ac4a93c6cb87b67
NusaStudio <116783164+NusaStudio@users.noreply.github.com>
2022-10-27 20:07:00 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -20,6 +20,6 @@ PRODUCT_MAKEFILES := \
      $(LOCAL_DIR)/omni_Infinix-X688C.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X688B-user \
-    omni_Infinix-X688B-userdebug \
-    omni_Infinix-X688B-eng
+    omni_Infinix-X688C-user \
+    omni_Infinix-X688C-userdebug \
+    omni_Infinix-X688C-eng
