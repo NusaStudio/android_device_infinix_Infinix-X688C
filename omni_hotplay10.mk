dea698d8ccf121f44d8447d5202561dd096c6d3c
NusaStudio <116783164+NusaStudio@users.noreply.github.com>
2022-10-29 22:26:16 +0700

    Update and rename omni_Infinix-X688C.mk to omni_hotplay10.mk

--- omni_hotplay10.mk
+++ omni_hotplay10.mk
@@ -22,16 +22,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from Infinix-X688C device
-$(call inherit-product, device/infinix/Infinix-X688C/device.mk)
+$(call inherit-product, device/infinix/hotplay10/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Infinix-X688C
-PRODUCT_NAME := omni_Infinix-X688C
+PRODUCT_DEVICE := hotplay10
+PRODUCT_NAME := omni_hotplay10
 PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix
-PRODUCT_MANUFACTURER := infinix
-PRODUCT_RELEASE_NAME := Infinix Infinix
+PRODUCT_MODEL := Infinix X688C
+PRODUCT_MANUFACTURER := Infinix Mobile Limited
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
+	
+# Resolution
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1600
